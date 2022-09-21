82dc7c7f0d76b0855e22dbf47947721c37914a9c
ismaelyoussef1009 <ismaelyoussef1009>
2022-09-21 14:48:19 +0100

    test

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Which organization defines web standards ?",
+  answer: "World Wide Web Consortium",
+  options: [
+    "Apple Inc.",
+    "IBM Corporation",
+    "World Wide Web Consortium",
+    "Microsoft Corporation"
+  ]
+},
+{
+  numb: 2,
+  question: "HTML is considered ?",
+  answer: "Markup language",
+  options: [
+    "OOP language",
+    "Programming language",
+    "Markup language",
+    "High level language"
+  ]
+},
+{
+  numb: 3,
+  question: "HTML was first offered in what year ?",
+  answer: "1990",
+  options: [
+    "1945",
+    "1990",
+    "2000",
+    "1995"
+  ]
+},
+{
+  numb: 4,
+  question: "Who is the primary HTML author?",
+  answer: "Tim Berners-Lee",
+  options: [
+    "Brendan Eich",
+    "Web developer",
+    "Tim Berners-Lee",
+    "Google Inc"
+  ]
+},
+{
+  numb: 5,
+  question: "If we want to style only one element, which css selector will we use?",
+  answer: "id",
+  options: [
+    "the element tag",
+    "id",
+    "class",
+    "all of the above"
+  ]
+},
+{
+  numb: 6,
+  question: "The HTML tag that specifies an embedded CSS style in an element is called ?",
+  answer: "style",
+  options: [
+    "style",
+    "link",
+    "stylesheet",
+    "style.css"
+  ]
+},
+{
+  numb: 7,
+  question: "An HTML document can contain ?",
+  answer: "all of the above",
+  options: [
+    "attributes",
+    "tags",
+    "plain text",
+    "all of the above"
+  ]
+},
+{
+  numb: 8,
+  question: "If we want to wrap text around an image, which CSS property are we going to use?",
+  answer: "float",
+  options: [
+    "justify-content",
+    "align",
+    "float",
+    "all of the above"
+  ]
+},
+{
+  numb: 9,
+  question: "what are the ordered elements of a commit",
+  answer: "add-commit-pull-push",
+  options: [
+    "add-commit-push",
+    "add-commit-pull-push",
+    "Add-Commit-Pull-Push",
+    "the last two answers"
+  ]
+},
+{
+  numb: 10,
+  question: "the machine representation of 13 is ?",
+  answer: "1101",
+  options: [
+    "1001",
+    "1101",
+    "1100",
+    "none of the above"
+  ]
+},
+
+];
